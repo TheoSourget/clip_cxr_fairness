6c7b583b1f2e204b1c040844a86d9f4a7b1f0c27
TheoSourget <sourget.theo@live.fr>
2025-05-21 10:24:37 +0200

    Apply model for classification + embedding analysis on a single PCA/TSNE

--- apply_model.py
+++ apply_model.py
@@ -55,29 +55,31 @@ def save_embeddings(image_paths,label_texts,model,batch_size,save_file):
     np.save(f"{save_file}_images",embeddings['img_embeds'])
     np.save(f"{save_file}_texts",embeddings['text_embeds'])
 
+
+def save_probas(image_paths,label_texts,model,batch_size,save_file):
+    lst_probas = []
+    for i in tqdm.tqdm(range(len(image_paths)//batch_size),total=len(image_paths)//batch_size):
+        imgs_batch = image_paths[i*batch_size:(i+1)*batch_size]
+        
+        #Check whether text in lagel_texts are actual labels (then size of label_texts is different than the number of images) or reports (1 report per image)
+        if len(label_texts) != len(image_paths):
+            #Actual labels: same text for every batch
+            texts_batch = label_texts
+        else:
+            #Reports: the texts are also batched
+            texts_batch = label_texts[i*batch_size:(i+1)*batch_size]
+
+        probas = model.get_embeddings(imgs_batch,texts_batch)
+        print(probas)
+        break
     return
-
-
-def extract_text(report):
-    if "FINDINGS:" in report:
-        sentences = report.split("FINDINGS:")[-1].split(".")
-    elif "IMPRESSION:" in report:
-        sentences = report.split("IMPRESSION:")[-1].split(".")
-    else:
-        sentences = report.split(".")
-    
-    if len(sentences) == 1:
-        return sentences[0]
-     
-    
-    random_sentence_idx = np.random.randint(max(1,len(sentences)-3))
-    return ".".join(sentences[random_sentence_idx:random_sentence_idx+3])
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', default='medclip')
     parser.add_argument('--image_folder', default='./data/RSNA_png/')
     parser.add_argument('--batch_size', default=1, type=int)
+    parser.add_argument('--classification', default=False, type=bool)
 
     args, unknown = parser.parse_known_args()
 
@@ -89,18 +91,12 @@ def main():
     # image_paths = df_unique['path'].to_numpy()[:]
 
     #FOR MIMIC
-    mimic_path = './data/'
-    df = pd.read_csv(f'{mimic_path}test_preproc.csv')
+    mimic_path = '/gpfs/workdir/shared/cpm4c/datasets/MIMIC/mimic/'
+    df = pd.read_csv(f'{mimic_path}test_preproc_filtered.csv')
+    df = df.dropna(subset=["findings"])
     df['path_preproc'] = df['path_preproc'].apply(lambda img_path:f'{mimic_path}{img_path}')
-    image_paths = df['path_preproc'].to_numpy()[:]
-    label_texts = df['report'].apply(extract_text).to_list()[:]
-    df['processed_reports'] = label_texts
-    df.to_csv(f'{mimic_path}preproc_reports_mimic.csv')
-    return 
-    # label_texts = [
-    #     'Healthy',
-    #     'Abnormal',
-    # ]
+    image_paths = df['path_preproc'].to_numpy()[:32]
+
     with torch.no_grad():
         if args.model_name == 'medclip':
             model = MedCLIP()
@@ -118,10 +114,14 @@ def main():
             print('Unknown model name, choose in the following list: medclip,biovil,biovil-t,medimageinsight,chexzero,cxrclip')
             return
    
-        # result = model.get_predictions(image_paths,label_texts)
-        # result = model.get_embeddings(image_paths,label_texts)
-        save_embeddings(image_paths,label_texts,model,args.batch_size,f'./data/embeddings/MIMIC_{args.model_name}')
-        # print(result['img_embeds'],len(result['text_embeds']))
+        if args.classification:
+            label_texts = [
+                'No sign of abnormality',
+            ]
+            save_probas(image_paths,label_texts,model,args.batch_size,f'./data/probas/MIMIC/MIMIC_{args.model_name}')
+        else:
+            label_texts = df['findings'].to_list()[:]
+            save_embeddings(image_paths,label_texts,model,args.batch_size,f'./data/embeddings/MIMIC/MIMIC_{args.model_name}')
 
     
 if __name__ == "__main__":
