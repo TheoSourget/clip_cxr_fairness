b3d74d37f07a89513c87b292ce031f6a1ff8a7d1
TheoSourget <sourget.theo@live.fr>
2025-05-23 14:26:55 +0200

    Remove legacy code in embedding_analysis

--- embedding_analysis.py
+++ embedding_analysis.py
@@ -16,6 +16,7 @@ from PIL import Image
 import seaborn as sns
 import argparse
 from pathlib import Path
+np.random.seed(1907)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_name', default='medclip')
@@ -91,46 +92,4 @@ for charac in characteristics:
     fig.ax_joint.legend(loc='upper right')
     plt.xlabel(f"{projection_type} 1")
     plt.ylabel(f"{projection_type} 2")
-    plt.savefig(f"./reports/figures/{projection_type}/{model_name}/{projection_type}_{model_name}_{charac.lower()}_texts.png", bbox_inches='tight', dpi=300)
-
-
-# plt.colorbar()
-# create the annotations box
-# # Generate data x, y for scatter and an array of images.
-# arr = np.empty((1024,1024))
-# im = OffsetImage(arr, cmap='gray', zoom=5)
-# xybox=(50., 50.)
-# ab = AnnotationBbox(im, (0,0), xybox=xybox, xycoords='data',
-#         boxcoords="offset points",  pad=0.3,  arrowprops=dict(arrowstyle="->"))
-# # add it to the axes and make it invisible
-# ax.add_artist(ab)
-# ab.set_visible(False)
-
-# def show_xray(event):
-#     ind = event.ind[0]
-#     w,h = fig.get_size_inches()*fig.dpi
-#     ws = (pca_embeddings[ind,0] > w/2.)*-1 + (pca_embeddings[ind,0] <= w/2.) 
-#     hs = (pca_embeddings[ind,1] > h/2.)*-1 + (pca_embeddings[ind,1] <= h/2.)
-#     # if event occurs in the top or right quadrant of the figure,
-#     # change the annotation box position relative to mouse.
-#     ab.xybox = (xybox[0]*ws, xybox[1]*hs)
-#     # make annotation box visible
-#     ab.set_visible(True)
-#     # place it at the position of the hovered scatter point
-#     ab.xy =(pca_embeddings[ind,0], pca_embeddings[ind,1])
-#     # set the image corresponding to that point
-    
-#     path = f'./data/RSNA_png/{df_unique["patientId"].iloc[ind]}.png'
-#     img=Image.open(path)
-#     img.thumbnail((100,100),Image.LANCZOS)
-#     im.set_data(img)
-#     fig.canvas.draw_idle()
-
-# def hide_xray(event):
-#     if event.key == 'escape':
-#         ab.set_visible(False)
-#         fig.canvas.draw_idle()
-
-# # add callback for mouse moves
-# fig.canvas.mpl_connect('pick_event', show_xray)  
-# fig.canvas.mpl_connect('key_press_event', hide_xray)           
+    plt.savefig(f"./reports/figures/{projection_type}/{model_name}/{projection_type}_{model_name}_{charac.lower()}_texts.png", bbox_inches='tight', dpi=300) 
