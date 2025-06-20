a8bd2e66d845db3cc9ade8de673403c23931cebc
TheoSourget <sourget.theo@live.fr>
2025-06-20 10:05:51 +0200

    comments evaluate_performance

--- evaluate_performance.py
+++ evaluate_performance.py
@@ -21,12 +21,16 @@ np.random.seed(1907)
 def compute_probas(image_paths,label_texts,model,batch_size):
     lst_probas = {label:[] for label in label_texts}
     for i in tqdm.tqdm(range(len(image_paths)//batch_size),total=len(image_paths)//batch_size):
+        #Get paths of images for this batch
         imgs_batch = image_paths[i*batch_size:(i+1)*batch_size]
+
         for label in label_texts:
+            #Probabilities for a label will be computed using the comparison to the "Chest No findings" text.
             templates = [f"Chest {label}",f"Chest No findings"]
             probas = model.get_predictions(imgs_batch,templates)
             lst_probas[label] = np.append(lst_probas[label],np.array(probas)[:,0])
 
+    #Extra steps if needed
     if (len(image_paths)%batch_size != 0) and (len(image_paths)>batch_size):
         imgs_batch = image_paths[len(image_paths)-(len(image_paths)%batch_size):]
         for label in label_texts:
@@ -61,6 +65,7 @@ def main():
 
     args, unknown = parser.parse_known_args()
 
+    #Load the dataset and define which labels and attributes to use for the evaluation
     #FOR MIMIC
     if args.dataset == "MIMIC":
         mimic_path = './data/'
@@ -85,10 +90,11 @@ def main():
             'Pneumothorax',
         ]
         attributes = ["Drain"]
-
-
     image_paths = df['path_preproc'].to_numpy()[:]
+
+    
     with torch.no_grad():
+        #Load the model
         if args.model_name == 'medclip':
             model = MedCLIP()
         elif args.model_name == 'biovil':
@@ -104,25 +110,30 @@ def main():
         else:
             print('Unknown model name, choose in the following list: medclip,biovil,biovil-t,medimageinsight,chexzero,cxrclip')
             return
-   
+    
+        #Get the probabilities for each sample
         print("Computing predictions")
         lst_probas = compute_probas(image_paths,labels,model,args.batch_size)
         for label in labels:
             df[f"proba_{label}"] = lst_probas[f"{label}"]
         
+        #Save the probabilities
         Path(f"./data/probas_{args.dataset}/").mkdir(parents=True, exist_ok=True)
         df.to_csv(f"./data/probas_{args.dataset}/probas_{args.dataset}_{args.model_name}.csv")
 
+        #Load the proababilites (in case you saved them before, you can comment line 97-122)
         df = pd.read_csv(f"./data/probas_{args.dataset}/probas_{args.dataset}_{args.model_name}.csv")
         if args.dataset == "MIMIC":
             df["age"] = df["age"].astype(int)
             df["age_group"] = pd.cut(df["age"],bins=[18,25,50,65,80,np.inf],labels=["18-25","25-50","50-65","65-80","80+"],right=False)
             df = df.sort_values(by=["age"])
-            
+        
+        #Compute the AUC and AUPRC and confidence interval for the whole dataset and then per the subgroups in the attribute defined before
         Path(f"./data/performance/{args.dataset}/").mkdir(parents=True, exist_ok=True)
         with open(f"./data/performance/{args.dataset}/zeroshot_{args.model_name}.csv","w") as perf_file:
             perf_file.write("class,group,AUC,CI_AUC_low,CI_AUC_up,AUPRC,CI_AUPRC_low,CI_AUPRC_up")
             for label in labels:
+                #Global performance per label
                 y_true = df[label].fillna(0).to_numpy()
                 y_proba = df[f"proba_{label}"].to_numpy()
                 auc = roc_auc_score(y_true,y_proba)
@@ -130,6 +141,8 @@ def main():
                 ci_auc = bootstrap(y_true,y_proba,roc_auc_score)
                 ci_auprc = bootstrap(y_true,y_proba,average_precision_score)
                 perf_file.write(f"\n{label},global,{round(auc,2)},{round(ci_auc[0],2)},{round(ci_auc[1],2)},{round(auprc,2)},{round(ci_auprc[0],2)},{round(ci_auprc[1],2)}")
+                
+                #Performance per label and subgroup
                 for attribute in attributes:
                     attribute_values = df[attribute].unique()
                     for subgroup in attribute_values:
